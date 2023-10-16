dc3b0290ed69be44b8978313101306703e83d7e5
Thomas Randall <36749093+tlranda@users.noreply.github.com>
2023-10-15 23:46:32 -0400

    Update Module
    Usually fixes import errors

--- GC_TLA/__init__.py
+++ GC_TLA/__init__.py
@@ -2,3 +2,19 @@
 Benchmarks are here to help you reproduce our results.
 They can also be used to test your installation of ytopt or to discover the many parameters of a search.
 """
+
+"""
+Benchmarks are here to help you reproduce our results.
+They can also be used to test your installation of ytopt or to discover the many parameters of a search.
+"""
+__version__ = 1.0
+__name__ = 'GC_TLA'
+
+from . import base_plopper
+#from .base_plopper import (findReplaceRegex, Plopper, LibE_Plopper, ECP_Plopper, Polybench_Plopper,
+#                           Dummy_Plopper, LazyPlopper, )
+from . import base_problem
+#from .base_problem import (setWhenDefined, BaseProblem, import_method_builder, libe_problem_builder,
+#                           dummy_problem_builder, ecp_problem_builder, polybench_problem_builder, )
+from . import gc_tla_utils
+#from .gc_tla_utils import (load_from_problem, load_problem_module, load_without_problem, )
