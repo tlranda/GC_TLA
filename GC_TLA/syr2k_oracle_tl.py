e7b07029ce9673f4746a0f75b4f98c6d4255b66c
Thomas L Randall <tlranda@clemson.edu>
2024-01-30 14:54:03 -0500

    Fix SDV for repeatable/changeable sampling RNG

--- GC_TLA/syr2k_oracle_tl.py
+++ GC_TLA/syr2k_oracle_tl.py
@@ -38,8 +38,10 @@ def build():
     experiment = parser.add_argument_group("Experiment")
     experiment.add_argument("--max-evals", type=int, default=30,
                             help="Number of evals (default: %(default)s)")
-    experiment.add_argument("--seed", type=int, default=None,
-                            help="RNG seed (default: %(default)s)")
+    experiment.add_argument("--fit-seed", type=int, default=None,
+                            help="RNG seed during model fitting (default: %(default)s)")
+    experiment.add_argument("--sample-seed", type=int, default=None, nargs="*",
+                            help="RNG seed(s) during model inference (default: %(default)s)")
     data = parser.add_argument_group("Data")
     data.add_argument("--top", type=float, default=0.3,
                             help="Filtering quantile (default: %(default)s)")
@@ -55,7 +57,7 @@ def build():
     files.add_argument("--transfer-scale", required=True,
                             help="Scale identifier to predict against")
     files.add_argument("--output", default="oracle_gc.csv",
-                            help="Path to save results to (default %(default)s)")
+                            help="Path to save results to (if --sample-seed is provided, filename will automatically be noted with each seed value) (default %(default)s)")
     budget = parser.add_argument_group("Budget Estimation")
     budget.add_argument("--skip-auto-budget", action="store_true",
                             help="Always use --max-evals as budet, do not attempt to derive an experiment budget (default: %(default)s)")
@@ -77,11 +79,13 @@ def parse(args=None, prs=None):
     if len(args.input_data) != len(args.input_attr):
         raise ValueError(f"Must supply one --input-attr (observed: {len(args.input_attr)}) per --input-data (observed: {len(args.input_data)})")
     # Seed handling
-    if args.seed is not None:
-        np.random.seed(args.seed)
+    if args.fit_seed is not None:
+        np.random.seed(args.fit_seed)
         # SDV can use torch in lower-level modules
         import torch
-        torch.manual_seed(args.seed)
+        torch.manual_seed(args.fit_seed)
+    if args.sample_seed is None:
+        args.sample_seed = [None]
     return args
 
 def import_hook_from_file(fname, attr):
@@ -158,6 +162,7 @@ def main(args=None):
         budget_conditions = [Condition({'scale': evaluator.problem_class}, num_rows=initial_population)]
         print(f"Determining auto-budget...")
         massive_sample = model.sample_from_conditions(budget_conditions)
+        model.reset_sampling() # Ensure budgeting does not change RNG for the observed output (especially if skipped)
         sampled_pop_size = len(massive_sample.drop_duplicates())
         ideal = int(initial_population * args.budget_ideal)
         remaining_ideal = max(1, ideal - int((initial_population - sampled_pop_size) * args.budget_attrition))
@@ -183,15 +188,41 @@ def main(args=None):
                 suggested_budget = args.max_evals
     # Actual runtime experiment (using oracle)
     eval_conditions = [Condition({'scale': evaluator.problem_class}, num_rows=suggested_budget)]
-    conditional_samples = model.sample_from_conditions(eval_conditions)
-    results = pd.DataFrame([], columns=training_data.columns)
-    for idx, row in conditional_samples.iterrows():
-        row_params = [_ for _ in row[:-1]]
-        objective = evaluator.oracle_search(row_params)
-        row_data = dict((c,d) for (c,d) in row.items())
-        row_data['objective'] = objective
-        results = pd.concat((results,pd.DataFrame(row_data, index=[0]))).reset_index(drop=True)
-    results.to_csv(args.output, index=False)
+    for sample_seed in args.sample_seed:
+        if sample_seed is not None:
+            # ---- UGLY WORKAROUND -----
+            # SDV forces a fixed RNG seed every time you sample with conditions :(
+            #
+            # The public API only provides .reset_sampling() to alter RNG, which returns RNG to the post-fitting state, which is ... insufficient.
+            # This only allows you to repeat the same RNG for different conditions, not to repeat RNG or alter RNG.
+            # As you can see, we use .reset_sampling() above for good reason, but this will not permit us all of the RNG control necessary to interact
+            # with the learned distribution in statistically analyzable manners.
+            # We need actually different RNG with the same condition / data for variance analysis (and many other use cases),
+            # but there currently isn't an "accepted" API to achieve this.
+            #
+            # As such, the next line of code is VERY unstable and SDV may deprecate or break it at any time without warning.
+            # As is often the case, SDV fixed this in the past and un-fixed it later, so this can be considered evolving behavior
+            # in other words: behavior is unpredictable based on your SDV installation version. Caution is advised.
+            # The current line of code in this file uses the user-level SDV object as that should remain SLIGHTLY more stable,
+            # but the intended end effector is buried in the secondary sub-model which can be represented most directly as:
+            #
+            # model._model.random_state.seed(sample_seed)
+            # ----- END DISCUSSION ------
+            model._set_random_state(sample_seed)
+            # Customize output name
+            out_name = pathlib.Path(args.output)
+            out_name = out_name.parents[0].joinpath(out_name.stem+f"_seed_{sample_seed}"+out_name.suffix)
+        else:
+            out_name = args.output
+        conditional_samples = model.sample_from_conditions(eval_conditions)
+        results = pd.DataFrame([], columns=training_data.columns)
+        for idx, row in conditional_samples.iterrows():
+            row_params = [_ for _ in row[:-1]]
+            objective = evaluator.oracle_search(row_params)
+            row_data = dict((c,d) for (c,d) in row.items())
+            row_data['objective'] = objective
+            results = pd.concat((results,pd.DataFrame(row_data, index=[0]))).reset_index(drop=True)
+        results.to_csv(out_name, index=False)
 
 if __name__ == '__main__':
     main()
