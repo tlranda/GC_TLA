e30ab9b3ae4cb148d2169464f771cfda1dc8c51d
Thomas L Randall <tlranda@clemson.edu>
2024-03-11 14:47:24 -0400

    Let's call this 2.0 even though it may continue to evolve

--- GC_TLA/__init__.py
+++ GC_TLA/__init__.py
@@ -1,20 +1,3 @@
-"""
-Benchmarks are here to help you reproduce our results.
-They can also be used to test your installation of ytopt or to discover the many parameters of a search.
-"""
-__version__ = 1.0
+__version__ = 2.0
 __name__ = 'GC_TLA'
-
-#from . import libE_asktell
-#from .libE_asktell import persistent_model
-#from . import libE_obj
-#from .libE_obj     import init_obj
-#from . import base_plopper
-#from .base_plopper import (findReplaceRegex, Plopper, LibE_Plopper, ECP_Plopper, Polybench_Plopper,
-#                           Dummy_Plopper, LazyPlopper, )
-#from . import base_problem
-#from .base_problem import (setWhenDefined, BaseProblem, import_method_builder, libe_problem_builder,
-#                           dummy_problem_builder, ecp_problem_builder, polybench_problem_builder, )
-#from . import gc_tla_utils
-#from .gc_tla_utils import (load_from_problem, load_problem_module, load_without_problem, )
 
